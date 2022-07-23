2e72c880cffcb2744593646be13d2e1c21e6775f
kristafrancis721 <kristafrancis08@yahoo.com>
2022-07-23 14:03:29 -0400

    added marker

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,12 @@ let orlandoLatLng = { lat: 28.538336, lng: -81.379234 }
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: orlandoLatLng,
-    zoom: 8,
+    zoom: 11,
   });
-}
 
+
+const marker = new google.maps.Marker({
+  position: orlandoLatLng, 
+  map: map,
+});
+}
